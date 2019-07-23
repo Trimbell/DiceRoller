752a65b21afc7a28510da6bd3e31eb703562ba3e
Mason <mtmaster3@gmail.com>
2019-07-23 09:05:09 -0500

    Add info text

--- app/src/main/java/com/trimbell/diceroller/MainActivity.kt
+++ app/src/main/java/com/trimbell/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val drawableResource1 = when (Random.nextInt(6) + 1) {
+        val diceNum1 = Random.nextInt(6) + 1
+        val diceNum2 = Random.nextInt(6) + 1
+
+        val drawableResource1 = when (diceNum1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -28,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val drawableResource2 = when (Random.nextInt(6) + 1) {
+        val drawableResource2 = when (diceNum2) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -37,8 +41,11 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
+
+        val diceText = findViewById<TextView>(R.id.dice_text)
         val diceImage1: ImageView = findViewById(R.id.dice_image_1)
         val diceImage2: ImageView = findViewById(R.id.dice_image_2)
+        diceText.text = getString(R.string.roll_info, diceNum1 + diceNum2)
         diceImage1.setImageResource(drawableResource1)
         diceImage2.setImageResource(drawableResource2)
     }
