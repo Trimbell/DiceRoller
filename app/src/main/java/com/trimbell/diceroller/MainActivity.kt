c4b4e7234b29f777509edb38205776d4093e591b
Mason Trimbell <mtmaster3@gmail.com>
2019-07-29 13:53:42 -0500

    Change to probability for doubles

--- app/src/main/java/com/trimbell/diceroller/MainActivity.kt
+++ app/src/main/java/com/trimbell/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.trimbell.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,40 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        val diceText = findViewById<TextView>(R.id.dice_text)
+        val diceImage1 = findViewById<ImageView>(R.id.dice_image_1)
+        val diceImage2 = findViewById<ImageView>(R.id.dice_image_2)
+
         rollButton.setOnClickListener {
-            rollDice()
+            var count = 0
+            val rolls = 1000
+            repeat(rolls) {
+                if (rollDie(diceImage1) == rollDie(diceImage2)) count++
+            }
+            diceText.text = getString(R.string.roll_info, (count / rolls.toFloat() * 100f))
+//            diceText.text = "${0= * 100f}"
         }
     }
 
-    private fun rollDice() {
-        val diceNum1 = Random.nextInt(6) + 1
-        val diceNum2 = Random.nextInt(6) + 1
+    private fun rollDie(image: ImageView): Int {
+        val diceNum = Random.nextInt(6) + 1
 
-        val drawableResource1 = when (diceNum1) {
+        image.setImageResource(when (diceNum) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
-        }
+        })
 
-        val drawableResource2 = when (diceNum2) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-
-        val diceText = findViewById<TextView>(R.id.dice_text)
-        val diceImage1: ImageView = findViewById(R.id.dice_image_1)
-        val diceImage2: ImageView = findViewById(R.id.dice_image_2)
-        diceText.text = getString(R.string.roll_info, diceNum1 + diceNum2, diceNum1, diceNum2)
-        diceImage1.setImageResource(drawableResource1)
-        diceImage2.setImageResource(drawableResource2)
+        return diceNum
     }
 }
